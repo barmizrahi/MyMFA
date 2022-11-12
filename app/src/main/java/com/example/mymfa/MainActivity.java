258521b9a5ae6a9761000ace6b580919e3e9a654
bar mizrahi <Barm471@gmail.com>
2022-11-12 18:30:33 +0200

    password + voice + contact + battery + airPlane Mode works

--- app/src/main/java/com/example/mymfa/MainActivity.java
+++ app/src/main/java/com/example/mymfa/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.mymfa;
 
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
@@ -9,25 +13,18 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.media.MediaPlayer;
-import android.media.MediaRecorder;
 import android.os.BatteryManager;
 import android.os.Build;
-import android.os.Bundle;
 import android.os.Environment;
+import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textview.MaterialTextView;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,19 +35,20 @@ public class MainActivity extends AppCompatActivity {
     private MaterialButton request;
     private MaterialButton stop;
     private TextInputEditText passWord;
-    AudioChecker audioChecker;
+    private AudioChecker audioChecker;
 
-    private String names;
-    private MediaRecorder recorder;
-    private MediaPlayer mediaPlayer;
-    private String path;
-    private boolean boolRecord;
+    //private String names;
+    //private MediaRecorder recorder;
+    //private MediaPlayer mediaPlayer;
+    // private String path;
+    //private boolean boolRecord;
     private boolean boolContact;
     private boolean boolBattery;
     private boolean boolPass;
+    private boolean boolAirPlane;
 
     private final int REQUEST_CODE_PERMISSION_CONTACTS = 900;
-    private final int REQUEST_CODE_PERMISSION_CAMERA = 901;
+    //private final int REQUEST_CODE_PERMISSION_CAMERA = 901;
     private final int REQUEST_CODE_PERMISSION_WRITE_EXTERNAL_STORAGE = 902;
     private final int REQUEST_CODE_PERMISSION_MICROPHONE = 903;
     private final String PASSWORD = "1";
@@ -70,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void refresh() {
+    private void Enter() {
         boolean result = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED;
         boolean r1 =  ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED;
         boolean r2 =  ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
@@ -79,9 +77,8 @@ public class MainActivity extends AppCompatActivity {
             audioChecker.getVoice();
             audioChecker.playVoice();
             checkPassWord();
+            checkAirplaneMode();
         }
-
-        
     }
 
     private void checkPassWord() {
@@ -89,6 +86,17 @@ public class MainActivity extends AppCompatActivity {
             boolPass = true;
         else
             boolPass = false;
+    }
+
+    private void checkAirplaneMode() {
+        if (isAirplaneModeOn(getApplicationContext())) {
+            boolAirPlane = true;
+        } else {
+            boolAirPlane = false;
+        }
+    }
+    private boolean isAirplaneModeOn(Context context) {
+        return Settings.System.getInt(context.getContentResolver(), Settings.Global.AIRPLANE_MODE_ON, 0) != 0;
     }
 
     private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver(){
@@ -110,17 +118,17 @@ public class MainActivity extends AppCompatActivity {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD_MR1) {
             audioChecker.setPath(this.getExternalFilesDir(Environment.DIRECTORY_DCIM) + "/"  + "recordingAudio.mp3");
-           // path = this.getExternalFilesDir
-                   // (Environment.DIRECTORY_DCIM) + "/"  + "recordingAudio.mp3";
+            // path = this.getExternalFilesDir
+            // (Environment.DIRECTORY_DCIM) + "/"  + "recordingAudio.mp3";
         }
         else
         {
             audioChecker.setPath(Environment.getExternalStorageDirectory()
                     .toString() + "/"  + "recordingAudio.mp3");
-           // path=Environment.getExternalStorageDirectory()
-                 //   .toString() + "/"  + "recordingAudio.mp3";
+            // path=Environment.getExternalStorageDirectory()
+            //   .toString() + "/"  + "recordingAudio.mp3";
         }
-      //  path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + "recordingAudio.mp3";
+        //  path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + "recordingAudio.mp3";
       /*
         recorder = new MediaRecorder();
         recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
@@ -143,7 +151,7 @@ public class MainActivity extends AppCompatActivity {
         audioChecker.getMediaRecorder().release();
         playVoice();
         stop.setVisibility(View.GONE);
-        if(boolContact && audioChecker.getBoolRecord() && boolBattery && boolPass)
+        if(boolContact && audioChecker.getBoolRecord() && boolBattery && boolPass && boolAirPlane)
         {
             Toast.makeText(MainActivity.this, "Access Granted", Toast.LENGTH_SHORT).show();
         }
@@ -156,40 +164,37 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void requestAllPermissions() {
-       // ContactRequest();
-       // MicRequest();
+        // ContactRequest();
+        // MicRequest();
         //StorageRequest();
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS,Manifest.permission.RECORD_AUDIO,Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_PERMISSION_CONTACTS); //request for contact
         //ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CODE_PERMISSION_CAMERA);
         //ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_CODE_PERMISSION_MICROPHONE);//request for mircrophone
-       // ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_PERMISSION_WRITE_EXTERNAL_STORAGE);//request for mircrophone
+        // ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_PERMISSION_WRITE_EXTERNAL_STORAGE);//request for mircrophone
     }
-/*
-    private void StorageRequest() {
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_PERMISSION_WRITE_EXTERNAL_STORAGE);
-    }
+    /*
+        private void StorageRequest() {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_PERMISSION_WRITE_EXTERNAL_STORAGE);
+        }
 
-    private void MicRequest() {
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_CODE_PERMISSION_MICROPHONE);
-    }
+        private void MicRequest() {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_CODE_PERMISSION_MICROPHONE);
+        }
 
-    private void ContactRequest() {
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, REQUEST_CODE_PERMISSION_CONTACTS);
-    }
+        private void ContactRequest() {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, REQUEST_CODE_PERMISSION_CONTACTS);
+        }
 
-*/
+    */
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode) {
             case REQUEST_CODE_PERMISSION_CONTACTS: {
-                //Log.d("pttt", "REQUEST_CODE_PERMISSION_CONTACTS");
 
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                   // refresh();
                     Toast.makeText(MainActivity.this, "Contacts ok", Toast.LENGTH_SHORT).show();
                 } else {
-                    //requestContacts();
                     Toast.makeText(MainActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
                 }
                 break;
@@ -200,7 +205,6 @@ public class MainActivity extends AppCompatActivity {
                 if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
                 {
                     Log.d("pttt", "REQUEST_CODE_PERMISSION_MICROPHONE");
-                 //   refresh();
                     Toast.makeText(MainActivity.this, "Mic ok", Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(MainActivity.this, "Permission denied to your Mic", Toast.LENGTH_SHORT).show();
@@ -212,7 +216,6 @@ public class MainActivity extends AppCompatActivity {
                 if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
                 {
                     Log.d("pttt", "REQUEST_CODE_PERMISSION_WRITE_EXTERNAL_STORAGE");
-                    //   refresh();
                     Toast.makeText(MainActivity.this, "storage ok", Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(MainActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
@@ -231,7 +234,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initViews() {
-        refresh.setOnClickListener(v -> refresh());
+        refresh.setOnClickListener(v -> Enter());
         stop.setOnClickListener(v -> StopListen());
     }
 
@@ -247,20 +250,20 @@ public class MainActivity extends AppCompatActivity {
         Cursor cursor = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
         if (cursor.getCount() > 0) {
             while (cursor.moveToNext()) {
-                @SuppressLint("Range") int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)));
+                @SuppressLint("Range")
+                int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)));
                 if (hasPhoneNumber > 0) {
                     ContactsInfo contactsInfo = new ContactsInfo();
                     contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
                     displayName = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                    contactsInfo.setContactId(contactId);
+                    contactsInfo.setDisplayName(displayName);
                     if (displayName.equals("אבא עבודה"))
                     {
-                        info.setText("ok");
+                        // info.setText("ok");
                         boolContact = true;
                         return;
                     }
-
-                    contactsInfo.setContactId(contactId);
-                    contactsInfo.setDisplayName(displayName);
                     Cursor phoneCursor = getContentResolver().query(
                             ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
                             null,
@@ -284,27 +287,3 @@ public class MainActivity extends AppCompatActivity {
         cursor.close();
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
